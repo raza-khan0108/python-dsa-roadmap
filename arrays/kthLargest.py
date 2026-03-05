b6d7bbe15dc9d0264fb4203ecf995b4a3352e125
coffeecoder08 <razawarsi0108@gmail.com>
2026-03-05 23:42:07 +0530

    k-th largest element

---
+++ arrays/kthLargest.py
@@ -0,0 +1,13 @@
+import heapq
+class Solution:
+    def findKthLargest(self,nums,k):
+        heap = []
+        for num in nums:
+            heapq.heappush(heap,num)
+
+            if len(heap) > k:
+                heapq.heappop(heap)
+        
+        return heap[0]
+
+print(Solution().findKthLargest([3, 2, 1, 5, 6, 4], 2))
