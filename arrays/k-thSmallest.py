ab1073f9d7a24cbfe4770acba63bcd3afd428292
coffeecoder08 <razawarsi828@gmail.com>
2026-03-05 23:58:02 +0530

    k-th Smallest element

---
+++ arrays/k-thSmallest.py
@@ -0,0 +1,14 @@
+import heapq
+class Solution:
+    def kthSmallest(self, arr, k):
+        max_heap = []
+        
+        for num in arr:
+            heapq.heappush(max_heap, -num)
+            
+            if len(max_heap) > k:
+                heapq.heappop(max_heap)
+        
+        return -max_heap[0]
+
+print(Solution().kthSmallest([3, 2, 1, 5, 6, 4], 3))
